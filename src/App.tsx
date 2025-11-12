e5a28e4251d465a03c7087b727d80dd83fdaa889
Tommy Phuong  <tphuong4@my.bcit.ca>
2025-11-11 18:00:41 -0800

    fixed context wrapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import MainLayout from '@layouts/MainLayout'
-import { CartProvider } from '@context/CartContext'
-import { AuthProvider } from '@context/AuthContext'
 
 function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <MainLayout>
-          <Outlet />
-        </MainLayout>
-      </CartProvider>
-    </AuthProvider>
+    <MainLayout>
+      <Outlet />
+    </MainLayout>
   )
 }
 
